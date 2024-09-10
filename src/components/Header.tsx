b08d44b3dfc0041cfc5bef3295d6d78e523d2dc5
minseogo <gominseo1112@gmail.com>
2024-09-10 16:13:42 +0900

    자기소개서 싱글페이지 컴포넌트 및 커스텀 작업 및 반응형완료 [1.2.0]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,67 +13,68 @@ const Header: React.FC = () => {
     const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
   return (
-    <div id="header" className=" d-flex flex-column mt-4 ms-4">
-        <div className=''>
-            <h1 className="sitename mb-3">Portfolio</h1>
-            <i className="header-toggle d-xl-none bi bi-list" onClick={toggleNavMenu}></i>
-            <div className='d-flex flex-row'>
-                <div className="profile-img col-6">
+    <div id="header" className=" d-flex flex-column mt-sm-4 mt-1 ms-sm-4 ms-2 me-2 me-sm-0">
+        <div>
+            <div className='dash_f ms-md-4 ms-0'>
+                <h1 className="sitename mb-3">Portfolio</h1>
+            </div>
+            <div className='d-flex flex-sm-row flex-column'>
+                <div className="profile-img col-sm-6">
                     <img src="/img/me.jpg" alt="" className="img-fluid" />
-                    <div className='info mt-3'>
-                        <p className='d-flex flex-column'>내 소개 간단하게 2-3줄 내 소개 간단하게 2-3줄내 소개 간단하게
-                            <span>내 소개 간단하게 2-3줄 내 소개 간단하게 2-3줄내 소개 간단하게</span>
-                            <span>내 소개 간단하게 2-3줄 내 소개 간단하게 2-3줄내 소개 간단하게</span>
+                    <div className='info mt-3 pe-4 pe-lg-0'>
+                        <p className='d-flex flex-column info_font text-sm-start text-center'>영국 어학연수와 MD 업무를 통해 소통 능력과 실무 역량을 키웠으며, 
+                            <span>프론트엔드 개발에 대한 흥미를 넓혔습니다.</span>
+                            <span>사용자 중심의 웹 경험을 제공하며 향후 프로젝트 성공에 기여하고 싶습니다. </span>
                         </p>
                     </div>
                 </div>
-                <div className="resume section d-flex flex-column col-5">
+                <div className="resume section d-flex flex-column col-sm-5">
                     <div className='hello'>
                     <h3 className='d-flex flex-column'>
+                        안녕하세요.
                         <ReactTyped
-                        strings={['프론트엔드 개발자', '웹퍼블리셔']}
-                        typeSpeed={50}
-                        backSpeed={30}
+                        strings={['프론트엔드 개발자', '웹 퍼블리셔']}
+                        typeSpeed={70}
+                        backSpeed={50}
                         loop
                         />
                         고민서입니다.
                     </h3>
                     </div>
-                    <div className='personal mb-5 mt-2'>
+                    <div className='personal mb-lg-5 mb-2 mt-2 text-sm-start text-center'>
                         <ul className='list-unstyled'>
                             <li>1994. 11. 12</li>
                             <li>010 - 5042 - 9412</li>
                             <li>gominseo1112@gmail.com</li>
                             <li>https://github.com/minseogo</li>
-                            <li>boggy-forgery-956</li>
                         </ul>
                     </div>
-                    <div className='skills mt-5 mb-4'>
-                        <h2 className="">What I Do</h2>
+                    <div className='skills mt-lg-5 mt-1 mb-lg-4 mb-0'>
+                        <h2 className="text-sm-start text-center">What I Do</h2>
                         <div className="d-flex flex-column align-items-stretch">
                             <div className="skills-content">
-                                <div className='d-flex align-items-center mt-1'>
-                                    <div className='text-center me-3'><img className='reactlogo' src={Reactsvg} alt="React svg" /></div>
-                                    <div className='text-center me-2'><img className='figmalogo' src={`${process.env.PUBLIC_URL}/img/Figma_logo.png`} alt="Figma png" /></div>
-                                    <div className='text-center me-2'><img src={`${process.env.PUBLIC_URL}/img/Cafe24_logo.png`} alt="Cafe24 png" /></div>
-                                    <div className='text-center'><img src={`${process.env.PUBLIC_URL}/img/Gnuboard_logo.png`} alt="Gnuboard png" /></div>
+                                <div className='d-flex flex-row mt-1 align-items-lg-center flex-lg-row justify-content-evenly align-items-sm-start flex-sm-column align-items-center justify-content-sm-start '>
+                                    <div className='text-lg-center text-start me-lg-3 me-1'><img className='reactlogo' src={Reactsvg} alt="React svg" /></div>
+                                    <div className='text-lg-center text-start me-lg-2 me-1 mt-lg-0 mt-sm-2'><img className='figmalogo' src={`${process.env.PUBLIC_URL}/img/Figma_logo.png`} alt="Figma png" /></div>
+                                    <div className='text-lg-center text-start me-lg-2 me-1 mt-lg-0 mt-sm-2'><img src={`${process.env.PUBLIC_URL}/img/Cafe24_logo.png`} alt="Cafe24 png" /></div>
+                                    <div className='text-lg-center text-start mt-lg-0 mt-sm-2 ms-md-0 ms-sm-1'><img src={`${process.env.PUBLIC_URL}/img/Gnuboard_logo.png`} alt="Gnuboard png" /></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='experience mt-5'>
+                    <div className='experience mt-lg-5 mt-2'>
                         {/* <h2>Experience</h2> */}
-                        <div className="d-flex">
-                            <div className='education'>
-                                <h3 className="resume-title">Education</h3>
+                        <div className="d-flex flex-lg-row flex-sm-column flex-column justify-content-sm-start justify-content-between align-items-sm-start align-items-center">
+                            <div className='education text-sm-start text-center'>
+                                <h3 className="resume-title text-sm-start text-center">Education</h3>
                                 <div className="resume-item">
                                 <h4>웹퍼블리셔(UI/UX)&프론트엔드개발</h4>
                                     <h5>2024.03 ~ 2024.09</h5>
                                 </div>
                                 <div className="resume-item d-flex flex-column">
                                     <h4>IH LONDON</h4>
-                                    <h5>2020.09 ~ 2020.11</h5>
-                                    <h5>2019.04 ~ 2019.07</h5>
+                                    <h5 className='pb-0 mb-0'>2020.09 ~ 2020.11</h5>
+                                    <h5 className='pb-2 mb-0'>2019.04 ~ 2019.07</h5>
                                     <p className='lo_font'><em>영국, 런던</em></p>
                                 </div>
                                 <div className="resume-item d-flex flex-column">
@@ -85,31 +86,31 @@ const Header: React.FC = () => {
                                     <h5>2010.03 ~ 2013.03</h5>
                                 </div>
                             </div>
-                            <div className="work pt-md-0 pt-5 ms-5">
+                            <div className="work pt-md-0 ms-lg-5 ms-0 text-sm-start text-center">
                                 <h3 className="resume-title">Work Experience</h3>
                                 <div className="resume-item">
                                     <h4>온라인 MD</h4>
-                                    <h5>2023.10 ~ 2024.01</h5>
+                                    <h5 className='pb-2 mb-0'>2023.10 ~ 2024.01</h5>
                                     <p className='lo_font'><em>웹뜰 / 대한민국, 서울</em></p>
                                 </div>
                                 <div className="resume-item">
                                     <h4>온라인 마케팅, 바리스타</h4>
-                                    <h5>2022.10 ~ 2023.05</h5>
+                                    <h5 className='pb-2 mb-0'>2022.10 ~ 2023.05</h5>
                                     <p className='lo_font'><em>Cocomelt / 영국, 런던</em></p>
                                 </div>
                                 <div className="resume-item">
                                     <h4>바리스타</h4>
-                                    <h5>2022.05 ~ 2022.08</h5>
+                                    <h5 className='pb-2 mb-0'>2022.05 ~ 2022.08</h5>
                                     <p className='lo_font'><em>Knockbox / 영국, 런던</em></p>
                                 </div>
                                 <div className="resume-item">
                                     <h4>온라인MD 인턴</h4>
-                                    <h5>2021.12 ~ 2022.03</h5>
+                                    <h5 className='pb-2 mb-0'>2021.12 ~ 2022.03</h5>
                                     <p className='lo_font'><em>Globalon / 영국, 런던</em></p>
                                 </div>
                                 <div className="resume-item">
                                     <h4>온라인MD</h4>
-                                    <h5>2021.03 ~ 2021.09</h5>
+                                    <h5 className='pb-2 mb-0'>2021.03 ~ 2021.09</h5>
                                     <p className='lo_font'><em>세븐제이스 / 대한민국, 경기도</em></p>
                                 </div>
                             </div>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -3,11 +3,11 @@ import '../css/main.css'
 
 const Portfolio: React.FC = () => {
   const portfolioItems = [
-    { id: 1, filter: '팀 프로젝트', image: 'veg.png', title: '팀 프로젝트', description: 'Lorem ipsum, dolor sit amet consectetur' },
-    { id: 2, filter: '팀 프로젝트', image: 'eclat.png', title: '팀 프로젝트', description: 'Lorem ipsum, dolor sit amet consectetur' },
-    { id: 3, filter: '개인 프로젝트', image: 'ck.png', title: '개인 프로젝트', description: 'Lorem ipsum, dolor sit amet consectetur' },
-    { id: 4, filter: '개인 프로젝트', image: 'ck.png', title: '개인 프로젝트', description: 'Lorem ipsum, dolor sit amet consectetur' },
-    { id: 5, filter: '개인 프로젝트', image: 'ck.png', title: '개인 프로젝트', description: 'Lorem ipsum, dolor sit amet consectetur' },
+    { id: 1, filter: '개인 프로젝트', image: 'ck.png', title: 'CK Gnuboard', description: '브랜드의 고급스러움과 미니멀리즘을 강조하며, 새로운 사용자 경험을 제공하기 위해 리뉴얼하고 그누보드를 기반으로 제작한 웹사이트입니다.', link: 'http://gominseo.dothome.co.kr/ck/'  },
+    { id: 2, filter: '개인 프로젝트', image: 'ck.png', title: 'CK Cafe24', description: '브랜드의 고급스러움과 미니멀리즘을 강조하며, 새로운 사용자 경험을 제공하기 위해 리뉴얼하고 카페24를 기반으로 제작한 웹사이트입니다.', link: 'https://minseogo.cafe24.com/'  },
+    { id: 3, filter: '개인 프로젝트', image: 'ck.png', title: 'CK', description: '브랜드의 고급스러움과 미니멀리즘을 강조하며, 새로운 사용자 경험을 제공하기 위해 리뉴얼하고 vercel 서버를 사용하여 제작한 웹사이트입니다.', link: 'https://gominseo-calvinklein.vercel.app/'  },
+    { id: 4, filter: '팀 프로젝트', image: 'veg.png', title: 'Ugly Us', description: '리뷰 컴포넌트를 개발 및 리뉴얼하여 사용자 맞춤형 경험과 리뷰 서비스를 제공하는 이커머스 웹사이트입니다.', link: 'https://team-project-reactuglyus.vercel.app/'  },
+    { id: 5, filter: '팀 프로젝트', image: 'eclat.png', title: 'Eclat', description: '브랜드 아이덴티티를 강화하고 사용자 친화적인 인터페이스를 제공하며, 시즌 추천, 이벤트, 리뷰 컴포넌트와 브랜드 스토리 페이지를 제작한 플랫폼형 웹사이트입니다.', link: 'https://i-web.kr/green05/'  },
   ];
 
   const [filter, setFilter] = React.useState<string>('All');
@@ -18,9 +18,8 @@ const Portfolio: React.FC = () => {
 
   return (
     <section id="portfolio" className="portfolio section light-background mt-4">
-      <div className="">
+      <div className="dash ms-4">
         <h1>Project</h1>
-
       </div>
 
       <div className="">
@@ -30,18 +29,20 @@ const Portfolio: React.FC = () => {
           <li onClick={() => setFilter('팀 프로젝트')} className={filter === '팀 프로젝트' ? 'filter-active' : ''}>팀 프로젝트</li>
         </ul>
 
-        <div className="row portfolio-container ms-5 me-2">
+        <div className="row portfolio-container ms-md-6 ms-0 me-2">
           {filteredItems.map(item => (
-            <div key={item.id} className="col-6 portfolio-item">
+            <div key={item.id} className="col-sm-6 portfolio-item">
               <div className="portfolio-content">
                 <img src={`/img/${item.image}`} className="img-fluid" alt={item.title} />
               </div>
-              <div className="portfolio-info">
-                  {/* <h4>{item.title}</h4> */}
+              <div className="portfolio-info mb-5">
+                <div className='d-flex flex-row justify-content-between align-items-center'>
+                  <h4 className='mb-0'>{item.title}</h4>
+                    <a href={item.link} className="details-link" target="_blank" rel="noopener noreferrer">
+                      <i className="bi bi-link-45deg text-black"></i>
+                    </a>
+                </div>
                   <p>{item.description}</p>
-                  <a href="portfolio-details.html" className="details-link">
-                    <i className="bi bi-link-45deg text-black"></i>
-                  </a>
                 </div>
             </div>
           ))}
